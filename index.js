cab1cdbcd728274678cd1abe99e588d2158ab963
PeterAronZentai <peter.zentai@avangrade.com>
2015-04-09 15:35:08 +0100

    Visual studio additions for gitignore

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Peter on 4/9/2015.
+ */
+
+var assert = require('assert');
+
+assert.ok(true, "true must be true");
+
+
+//https://github.com/tes/service-payment-daily-export/blob/master/src/fileRepo.js
