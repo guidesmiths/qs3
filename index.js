c276b972294ac2e05a441bdad826d864653f453e
PeterAronZentai <peter.zentai@avangrade.com>
2015-04-09 16:23:02 +0100

    finally "Access denied" resolved by - precreating the bucket ...I always forget how bad AWS API namings are -

--- index.js
+++ index.js
@@ -3,8 +3,24 @@
  */
 
 var assert = require('assert');
+var AWS = require('aws-sdk');
 
-assert.ok(true, "true must be true");
+
+AWS.config.region = 'eu-west-1';
+
+
+var s3bucket = new AWS.S3({ params: { Bucket: 'gs-test-field' } });
+
+s3bucket.createBucket(function () {
+    var params = { Key: 'myKey', Body: 'Hello!' };
+    s3bucket.upload(params, function (err, data) {
+        if (err) {
+            console.log("Error uploading data: ", err);
+        } else {
+            console.log("Successfully uploaded data to myBucket/myKey");
+        }
+    });
+});
 
 
 //https://github.com/tes/service-payment-daily-export/blob/master/src/fileRepo.js
